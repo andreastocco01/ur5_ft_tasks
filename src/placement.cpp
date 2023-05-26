b80b052d1ec16dda8ec479cf4b6feab46f756770
Andrea Stocco <astocco11@gmail.com>
2023-05-26 14:17:04 +0200

    update placement for finding the correct height to take the object. update movement in spiral_movement (now makes a spiral instead of multiple circles). update CMakeList

--- src/placement.cpp
+++ src/placement.cpp
@@ -1,20 +1,42 @@
 #include <ros/ros.h>
-#include <ros/topic.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
-#include <geometry_msgs/Point.h>
 #include <std_msgs/UInt16.h>
+#include <geometry_msgs/WrenchStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <signal.h>
+#include "ur5_ft_tasks/utils.h"
+#include "ur5_ft_tasks/controllers.h"
+#include "robotiq_ft_sensor/ft_sensor.h"
+#include "robotiq_ft_sensor/sensor_accessor.h"
 
 using namespace moveit::planning_interface;
 
+geometry_msgs::Vector3 force, torque;
 const std::string PLANNING_GROUP_ARM = "ur5_arm";
+controller_manager_msgs::SwitchController switch_srv;
+ros::ServiceClient switch_client;
 
 std::vector<geometry_msgs::PoseStamped> positions;
 
-void callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+void callback_pos(const geometry_msgs::PoseStamped::ConstPtr& msg) {
     positions.push_back(*msg);
 }
 
+void callback(const geometry_msgs::WrenchStamped::ConstPtr& msg) {
+    force = msg->wrench.force;
+    torque = msg->wrench.torque;
+    //ROS_INFO("I heard: fx[%f], fy[%f], fz[%f], tx[%f], ty[%f], tz[%f]", force.x, force.y, force.z, torque.x, torque.y, torque.z);
+}
+
+void mySigintHandler(int sig) {
+    // Do some custom action.
+    // For example, publish a stop message to some other nodes.
+
+    // All the default sigint handler does is call shutdown()
+    switch_controllers(switch_client, switch_srv, (char*)"scaled_pos_joint_traj_controller", (char*)"twist_controller");
+    ros::shutdown();
+}
 
 int main(int argc, char** argv){
 
@@ -22,7 +44,11 @@ int main(int argc, char** argv){
     ros::init(argc, argv, "placer");
     ros::NodeHandle node;
 
-    // Starting Async rosSpin() for moveit
+    // Override the default ros sigint handler.
+    // This must be set after the first NodeHandle is created.
+    signal(SIGINT, mySigintHandler);
+
+    // Starting Async spin() for moveit
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
@@ -36,14 +62,44 @@ int main(int argc, char** argv){
 
     // Setup topics
     ros::Publisher gripper_publisher = node.advertise<std_msgs::UInt16>("/gripper_control/servo_debug", 1);
-    ros::Subscriber pos_subscriber = node.subscribe("task_positions", 2, callback);
+    ros::Subscriber pos_subscriber = node.subscribe("task_positions", 2, callback_pos);
+    ros::Publisher publisher = node.advertise<geometry_msgs::Twist>("/twist_controller/command", 1);
+    ros::ServiceClient client = node.serviceClient<robotiq_ft_sensor::sensor_accessor>("robotiq_ft_sensor_acc");
+    ros::Subscriber subscriber = node.subscribe("robotiq_ft_wrench", 100, callback);
+    ros::ServiceClient load_client = node.serviceClient<controller_manager_msgs::LoadController>("/controller_manager/load_controller");
+    switch_client = node.serviceClient<controller_manager_msgs::SwitchController>("/controller_manager/switch_controller");
+    controller_manager_msgs::LoadController load_srv;
 
-    // Wait for message in /task_positions topic
+    // Wait for messages in /task_positions topic
     ROS_INFO("Waiting for positions");
 
     while(positions.size() < 2);
 
     ROS_INFO("Positions received");
+
+    robotiq_ft_sensor::sensor_accessor srv;
+    zero_sensor(srv, client);
+
+    load_controller(load_client, load_srv, (char*)"twist_controller");
+    switch_controllers(switch_client, switch_srv, (char*)"twist_controller", (char*)"scaled_pos_joint_traj_controller");
+
+    // Close gripper
+    gripper_position.data = 30;
+    gripper_publisher.publish(gripper_position);
+    sleep(1);
+
+    // Down until table_contact and save position
+    geometry_msgs::Twist move;
+    move.angular = set(0, 0, 0);
+    while(force.z > -8) {
+        move.linear = set(0, 0, -0.015);
+        publisher.publish(move);
+    }
+    move.linear = set(0, 0, 0);
+    publisher.publish(move);
+    geometry_msgs::PoseStamped table_height = move_group.getCurrentPose("robotiq_ft_frame_id");
+
+    switch_controllers(switch_client, switch_srv, (char*)"scaled_pos_joint_traj_controller", (char*)"twist_controller");
 
     // Move to first position
     positions[0].pose.position.z += 0.1;
@@ -51,19 +107,44 @@ int main(int argc, char** argv){
     move_group.move();  // Plan and execute movement waiting for success
     move_group.clearPoseTargets(); // Clear all targets specified
 
-    // TODO: descend
-    positions[0].pose.position.z -= 0.1;
-    move_group.setPoseTarget(positions[0].pose);
+    switch_controllers(switch_client, switch_srv, (char*)"twist_controller", (char*)"scaled_pos_joint_traj_controller");
+
+    // Down until object contact and save
+    while(force.z > -8) {
+        move.linear = set(0, 0, -0.015);
+        publisher.publish(move);
+    }
+    move.linear = set(0, 0, 0);
+    publisher.publish(move);
+    geometry_msgs::PoseStamped object_height = move_group.getCurrentPose("robotiq_ft_frame_id");
+
+    switch_controllers(switch_client, switch_srv, (char*)"scaled_pos_joint_traj_controller", (char*)"twist_controller");
+
+    // Little move up
+    object_height.pose.position.z += 0.01;
+    move_group.setPoseTarget(object_height.pose);
+    move_group.move();  // Plan and execute movement waiting for success
+    move_group.clearPoseTargets(); // Clear all targets specified
+
+    // Open gripper
+    gripper_position.data = 110;
+    gripper_publisher.publish(gripper_position);
+    sleep(1);
+
+    // Down to half object height
+    object_height.pose.position.z += 0.01;
+    object_height.pose.position.z -= ((object_height.pose.position.z - table_height.pose.position.z) / 2) + 0.02;
+    move_group.setPoseTarget(object_height.pose);
     move_group.move();  // Plan and execute movement waiting for success
     move_group.clearPoseTargets(); // Clear all targets specified
 
     // Close gripper
     gripper_position.data = 45;
     gripper_publisher.publish(gripper_position);
-    sleep(2);
+    sleep(1);
 
-    // Move to previous position
-    positions[0].pose.position.z += 0.1;
+    // Move to first position
+    positions[0].pose.position.z;
     move_group.setPoseTarget(positions[0].pose);
     move_group.move();  // Plan and execute movement waiting for success
     move_group.clearPoseTargets(); // Clear all targets specified
@@ -74,14 +155,6 @@ int main(int argc, char** argv){
     move_group.move(); // Plan and execute movement waiting for success
     move_group.clearPoseTargets(); // Clear all targets specified
 
-    positions[1].pose.position.z -= 0.7;
-    move_group.setPoseTarget(positions[1].pose);
-    move_group.move(); // Plan and execute movement waiting for success
-    move_group.clearPoseTargets(); // Clear all targets specified
-
-    // Open gripper
-    //gripper_position.data = 110;
-    //gripper_publisher.publish(gripper_position);
-    
+    mySigintHandler(SIGINT);
     return 0;
 }
--- src/spiral_movement.cpp
+++ src/spiral_movement.cpp
@@ -1,23 +1,15 @@
 #include <ros/ros.h>
-#include "ur5_ft_tasks/controllers.h"
-#include "robotiq_ft_sensor/ft_sensor.h"
-#include "robotiq_ft_sensor/sensor_accessor.h"
 #include <signal.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/WrenchStamped.h>
-#include <cmath>
+#include "robotiq_ft_sensor/ft_sensor.h"
+#include "robotiq_ft_sensor/sensor_accessor.h"
+#include "ur5_ft_tasks/utils.h"
+#include "ur5_ft_tasks/controllers.h"
 
 geometry_msgs::Vector3 force, torque;
 ros::ServiceClient switch_client;
 controller_manager_msgs::SwitchController switch_srv;
-
-void zero_sensor(robotiq_ft_sensor::sensor_accessor &srv, ros::ServiceClient &client) {
-    srv.request.command_id = srv.request.COMMAND_SET_ZERO;
-
-    if (client.call(srv)) {
-        ROS_INFO("Zeroing: %s", srv.response.res.c_str());
-    }
-}
 
 void callback(const geometry_msgs::WrenchStamped::ConstPtr& msg) {
     force = msg->wrench.force;
@@ -34,18 +26,10 @@ void mySigintHandler(int sig) {
     ros::shutdown();
 }
 
-geometry_msgs::Vector3 set(double x, double y, double z) {
-    geometry_msgs::Vector3 vector;
-    vector.x = x;
-    vector.y = y;
-    vector.z = z;
-
-    return vector;
-}
-
 int main(int argc, char** argv) {
     ros::init(argc, argv, "spiral");
     ros::NodeHandle node;
+
     // Override the default ros sigint handler.
     // This must be set after the first NodeHandle is created.
     signal(SIGINT, mySigintHandler);
@@ -70,31 +54,26 @@ int main(int argc, char** argv) {
     zero_sensor(srv, client);
 
     geometry_msgs::Twist move;
-    move.linear = set(0, 0, 0);
     move.angular = set(0, 0, 0);
 
-    while(force.z > -1) {
-        move.linear = set(0, 0, -0.02);
+    while(force.z > -8) {
+        move.linear = set(0, 0, -0.015);
         publisher.publish(move);
     }
     
     ros::Rate rate(72); // 5 secondi per fare un angolo giro
     double w = 0.25;
-    double radius = 0.05;
+    double radius = 0.01;
+    double angle = 0;
 
-    while(ros::ok && abs(force.x) < 7 && abs(force.y) < 7) {
-        for(int angle = 0; angle < 360; angle++) {
-            double rad = angle * M_PI / 180;
-            double v = radius * w;
-            if(force.z > -1) move.linear = set(v * sin(rad), v * cos(rad), -0.01);
-            else move.linear = set(v * sin(rad), v * cos(rad), 0);
-            if(abs(force.x) >= 7 || abs(force.y) >= 7) break;
-            publisher.publish(move);
-            ROS_INFO("%f, %f, %f", move.linear.x, move.linear.y, move.linear.z);
-            rate.sleep();
-        }
-        move.linear = set(0, 0, 0);
-        radius += 0.01;
+    while(ros::ok && force.z < -1) {
+        double rad = angle * M_PI / 180;
+        double v = radius * w;
+        move.linear = set(v * sin(rad), v * cos(rad), 0);
+        publisher.publish(move);
+        rate.sleep();
+        radius += 0.0001;
+        angle++;
     }
     mySigintHandler(SIGINT);
     return 0;
