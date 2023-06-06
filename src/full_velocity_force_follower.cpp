34431bcdc1ffd2625aae504df8cecfaf32873d3f
Andrea Stocco <astocco11@gmail.com>
2023-06-06 12:34:29 +0200

    fix full velocity force follower

--- src/full_velocity_force_follower.cpp
+++ src/full_velocity_force_follower.cpp
@@ -25,7 +25,7 @@ controller_manager_msgs::SwitchController switch_srv;
 void callback(const geometry_msgs::WrenchStamped::ConstPtr& msg) {
     force = msg->wrench.force;
     torque = msg->wrench.torque;
-    //ROS_INFO("I heard: fx[%f], fy[%f], fz[%f], tx[%f], ty[%f], tz[%f]", force.x, force.y, force.z, torque.x, torque.y, torque.z);
+    ROS_INFO("I heard: fx[%f], fy[%f], fz[%f], tx[%f], ty[%f], tz[%f]", force.x, force.y, force.z, torque.x, torque.y, torque.z);
 }
 
 void mySigintHandler(int sig) {
@@ -35,15 +35,6 @@ void mySigintHandler(int sig) {
     // All the default sigint handler does is call shutdown()
     switch_controllers(switch_client, switch_srv, (char*)"scaled_pos_joint_traj_controller", (char*)"twist_controller");
     ros::shutdown();
-}
-
-void feedback(ros::Publisher &publisher) {
-    std_msgs::UInt16 x;
-    x.data = 120;
-    publisher.publish(x);
-    ros::Duration(0.5).sleep();
-    x.data = 180;
-    publisher.publish(x);
 }
 
 int main(int argc, char** argv) {
@@ -74,8 +65,6 @@ int main(int argc, char** argv) {
     ros::ServiceClient client = node.serviceClient<robotiq_ft_sensor::sensor_accessor>("robotiq_ft_sensor_acc");
     ros::Subscriber subscriber = node.subscribe("robotiq_ft_wrench", 100, callback);
     ros::Publisher publisher = node.advertise<geometry_msgs::Twist>("/twist_controller/command", 1);
-    ros::Publisher pos_publisher = node.advertise<geometry_msgs::PoseStamped>("task_positions", 2);
-    ros::Publisher gripper_publisher = node.advertise<std_msgs::UInt16>("/gripper_control/servo_debug", 1);
     ros::ServiceClient load_client = node.serviceClient<controller_manager_msgs::LoadController>("/controller_manager/load_controller");
     switch_client = node.serviceClient<controller_manager_msgs::SwitchController>("/controller_manager/switch_controller");
     tf::TransformListener listener;
@@ -94,10 +83,6 @@ int main(int argc, char** argv) {
 
     zero_sensor(srv, client);
 
-    std_msgs::UInt16 init;
-    init.data = 180;
-    gripper_publisher.publish(init);
-
     geometry_msgs::Twist move;
     geometry_msgs::Vector3 linear, angular;
     linear = set(0.0, 0.0, 0.0);
@@ -108,23 +93,23 @@ int main(int argc, char** argv) {
 
     ros::Rate rate(100);
     int alpha = 175;
-    int threshold = 5;
-    std::vector<geometry_msgs::PoseStamped> positions;
+    int beta = 3;
+    double f_threshold = 5;
+    double t_threshold = 0.1;
     std::vector<geometry_msgs::Vector3> forces;
     std::vector<geometry_msgs::Vector3> torques;
     tf::StampedTransform transform;
 
-    while(ros::ok() && positions.size() < 3) {
+    while(ros::ok()) {
         torque.x = torque.y = 0;
 
         ros::Time mytime = ros::Time::now();
 
         try {
-             listener.lookupTransform("/base", "/robotiq_ft_frame_id",  
-                                     ros::Time(0), transform);
-         } catch (tf::TransformException ex) {
-             ROS_ERROR("%s",ex.what());
-             ros::Duration(1.0).sleep();
+            listener.lookupTransform("/base", "/robotiq_ft_frame_id", ros::Time(0), transform);
+        } catch (tf::TransformException ex) {
+            ROS_ERROR("%s",ex.what());
+            ros::Duration(1.0).sleep();
         }
 
         tf::Vector3 origin;
@@ -132,8 +117,6 @@ int main(int argc, char** argv) {
         origin.setY(0);
         origin.setZ(0);
         transform.setOrigin(origin);
-
-
 
         if(forces.size() < 20 && torques.size() < 20) {
             forces.push_back(force);
@@ -146,37 +129,10 @@ int main(int argc, char** argv) {
             geometry_msgs::Vector3 fcurrent = mean(forces);
             geometry_msgs::Vector3 tcurrent = mean(torques);
 
-
-            //geometry_msgs::Vector3Stamped fcurrent_stamped;
-            //fcurrent_stamped.header.stamp = ros::Time::now();
-            //fcurrent_stamped.header.frame_id = "/robotiq_ft_frame_id";
-            //fcurrent_stamped.vector = fcurrent;
-            // geometry_msgs::Vector3Stamped tcurrent_stamped;
-            // tcurrent_stamped.header.stamp = ros::Time::now();
-            // tcurrent_stamped.header.frame_id = "/robotiq_ft_frame_id";
-            // tcurrent_stamped.vector = tcurrent;
-
-
-            // Express force wrt base
-            //geometry_msgs::Vector3Stamped fcurrent_base;
-            //geometry_msgs::Vector3Stamped tcurrent_base;
-            //tf::Transformer::transformVector(std::string("/base"), fcurrent_stamped, fcurrent_base);
-            //tf::Transformer::transformVector(std::string("/base"), tcurrent_stamped, tcurrent_base);
-
-
             ros::Time now = ros::Time::now();
-            geometry_msgs::PointStamped fcurrent_base;
-            geometry_msgs::PointStamped fcurrent_stamped;
-            fcurrent_stamped.header.stamp = now;
-            fcurrent_stamped.header.frame_id = "/robotiq_ft_frame_id";
-            fcurrent_stamped.point.x = fcurrent.x;
-            fcurrent_stamped.point.y = fcurrent.y;
-            fcurrent_stamped.point.z = fcurrent.z;
-
+            geometry_msgs::Vector3 fcurrent_base;
 
             std::cout << "waiting TF...\n";
-            //listener.waitForTransform("/base", "/robotiq_ft_frame_id", now, ros::Duration(3.0));
-            //listener.transformPoint(std::string("/base"), fcurrent_stamped, fcurrent_base);
 
             tf::Vector3 fcurrent_tfvector;
             fcurrent_tfvector.setX(fcurrent.x);
@@ -185,26 +141,22 @@ int main(int argc, char** argv) {
 
             tf::Vector3 fcurrent_tfvector_out = transform * fcurrent_tfvector;
 
-            fcurrent_base.point.x = double(fcurrent_tfvector_out.x());
-            fcurrent_base.point.y= double(fcurrent_tfvector_out.y());
-            fcurrent_base.point.z = double(fcurrent_tfvector_out.z());
+            fcurrent_base.x = (double) fcurrent_tfvector_out.x();
+            fcurrent_base.y = (double) fcurrent_tfvector_out.y();
+            fcurrent_base.z = (double) fcurrent_tfvector_out.z();
 
             std::cout << fcurrent_base << std::endl;
-            std::cout << fcurrent_stamped << std::endl;
 
-
-
-
-            double fmodule = sqrt(pow(fcurrent.x, 2) + pow(fcurrent.y, 2) + pow(fcurrent.z, 2));
-            double tmodule = sqrt(pow(tcurrent.x, 2) + pow(tcurrent.y, 2) + pow(tcurrent.z, 2));
-            if(fmodule > threshold && tmodule > 0.1) {
-                linear = set(fcurrent_base.point.x/alpha, fcurrent_base.point.y/alpha, fcurrent_base.point.z/alpha);
-                angular = set(tcurrent.x, -tcurrent.y, -tcurrent.z);
-            } else if (tmodule > 0.1){
-                angular = set(tcurrent.x, -tcurrent.y, -tcurrent.z);
+            double fmodule = sqrt(pow(fcurrent_base.x, 2) + pow(fcurrent_base.y, 2) + pow(fcurrent_base.z, 2));
+            double tmodule = std::abs(tcurrent.z); // x and y always 0
+            if(fmodule > f_threshold && tmodule > t_threshold) {
+                linear = set(fcurrent_base.x/alpha, fcurrent_base.y/alpha, fcurrent_base.z/alpha);
+                angular = set(0, 0, -tcurrent.z/beta);
+            } else if (tmodule > t_threshold){
+                angular = set(0, 0, -tcurrent.z/beta);
                 linear = set(0, 0, 0);
-            } else if (fmodule > threshold) {
-                linear = set(fcurrent_base.point.x/alpha, fcurrent_base.point.y/alpha, fcurrent_base.point.z/alpha);
+            } else if (fmodule > f_threshold) {
+                linear = set(fcurrent_base.x/alpha, fcurrent_base.y/alpha, fcurrent_base.z/alpha);
                 angular = set(0, 0, 0);
             } else {
                 linear = set(0, 0, 0);
@@ -221,9 +173,6 @@ int main(int argc, char** argv) {
         ros::spinOnce();
         rate.sleep();
     }
-
-    pos_publisher.publish(positions[0]);
-    pos_publisher.publish(positions[1]);
 
     mySigintHandler(SIGINT);
     return 0;
